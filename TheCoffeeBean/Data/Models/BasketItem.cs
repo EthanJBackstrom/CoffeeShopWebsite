ec8ecfcc2af9055d60eb9f570425b2dfddfe3188
Ethan Backstrom <161535680+EthanJBackstrom@users.noreply.github.com>
2025-03-07 16:58:52 +0000

    4.0.1
    Minor Changes to Admin settings and Basket

--- TheCoffeeBean/Data/Models/BasketItem.cs
+++ TheCoffeeBean/Data/Models/BasketItem.cs
@@ -8,6 +8,7 @@ namespace TheCoffeeBean.Data.Models
 
     public class BasketItem
     {
+        
         public int BasketID { get; set; }
         public int StockID { get; set; }
         public int Quantity { get; set; } = 1;
--- TheCoffeeBean/Program.cs
+++ TheCoffeeBean/Program.cs
@@ -34,6 +34,7 @@ using (var scope = app.Services.CreateScope())
 }
 
 
+
 app.UseHttpsRedirection();
 app.UseStaticFiles();
 app.UseRouting();
