aed82a3359834de59e138d39c089a1550d6dab62
Ethan Backstrom <161535680+EthanJBackstrom@users.noreply.github.com>
2025-03-10 01:27:32 +0000

    5.1
    Fixed removing and editing on the admin menu.
    
    Implemented more comments
    
    Minor changes to code for ease of use and further progression

--- TheCoffeeBean/Pages/Admin/Admin.cshtml.cs
+++ TheCoffeeBean/Pages/Admin/Admin.cshtml.cs
@@ -9,7 +9,6 @@ using System.Threading.Tasks;
 
 namespace TheCoffeeBean.Pages.Admin
 
-
 {
     [Authorize(Roles = "Admin")] // Only useers with admin role is able toa ccess 
     public class AdminModel : PageModel
@@ -20,12 +19,16 @@ namespace TheCoffeeBean.Pages.Admin
         {
             _context = context;
         }
+
         // list of products 
         public IList<Product> Products { get; set; } = new List<Product>();
 
       
         [BindProperty]
         public Product NewProduct { get; set; } = new Product();
+
+        [BindProperty]
+        public Product EditProduct { get; set; }
 
         public async Task OnGetAsync()
         {
@@ -37,17 +40,63 @@ namespace TheCoffeeBean.Pages.Admin
         {
             if (!ModelState.IsValid)
             {
-                
                 Products = await _context.Products.ToListAsync();
                 return Page();
             }
             
             // adds new product to the database 
-
             _context.Products.Add(NewProduct);
             await _context.SaveChangesAsync();
+            return RedirectToPage();
+        }
 
-            
+        // Handles deleting a product
+        public async Task<IActionResult> OnPostDeleteAsync(int id)
+        {
+            var product = await _context.Products.FindAsync(id);
+            if (product != null)
+            {
+                _context.Products.Remove(product);
+                await _context.SaveChangesAsync();
+            }
+            return RedirectToPage();
+        }
+
+        // fetches the product
+        public async Task<IActionResult> OnPostEditAsync(int id)
+        {
+            EditProduct = await _context.Products.FindAsync(id);
+            if (EditProduct == null)
+            {
+                return NotFound();
+            }
+
+            Products = await _context.Products.ToListAsync();
+            return Page();
+        }
+
+        // Upadtee
+        public async Task<IActionResult> OnPostUpdateProductAsync()
+        {
+            if (!ModelState.IsValid)
+            {
+                Products = await _context.Products.ToListAsync();
+                return Page();
+            }
+
+            var productInDb = await _context.Products.FindAsync(EditProduct.Id);
+            if (productInDb == null)
+            {
+                return NotFound();
+            }
+
+            // Updates product 
+            productInDb.Name = EditProduct.Name;
+            productInDb.Description = EditProduct.Description;
+            productInDb.Price = EditProduct.Price;
+            productInDb.ImageUrl = EditProduct.ImageUrl;
+
+            await _context.SaveChangesAsync();
             return RedirectToPage();
         }
     }
